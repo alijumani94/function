c2b16c7eaa7c5f1ca4486a1458da4c3d48542cef
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-16 10:09:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function Ali() {
+    return "Hello Ali";
+}
+function abdullah() {
+    return ("hi");
+}
+var message = Ali();
+console.log(message);
+var sum = abdullah();
+console.log(sum);
---
+++ main.ts
@@ -0,0 +1,12 @@
+function Ali(){
+    return "Hello Ali";
+}
+function abdullah(){
+return ("hi");
+}
+
+let message= Ali();
+console.log(message);
+let sum= abdullah();
+console.log(sum);
+
